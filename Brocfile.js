5547d30cda57310425144b700aa370d4fbf21e8f
César Izurieta <cesar@caih.org>
2015-05-24 17:39:28 -0300

    Added admin-lte and customized

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+//app.import('bower_components/admin-lte/dist/js/app.js');
+app.import('bower_components/admin-lte/dist/css/AdminLTE.css');
+app.import('bower_components/admin-lte/dist/css/skins/skin-black.css');
+
 module.exports = app.toTree();
