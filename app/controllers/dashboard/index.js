97aceefa40e583b894c95500af9c39077fa2922a
César Izurieta <cesar@caih.org>
2015-06-01 01:03:31 -0300

    Create personal account if not present.

--- app/controllers/dashboard/index.js
+++ app/controllers/dashboard/index.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     createPersonalAccount() {
-      // TODO create personal account
+      var account = this.store.createRecord("account", {
+        name: this.get("username"),
+        description: "Personal Account"
+      });
+
+      account.save().then((account) => {
+        return this.get("session.currentUser").then((user) => {
+          return user.set("personalAccount", account).save();
+        });
+      }).catch(() => {
+        this.set("failed", true);
+      });
     }
   }
 });
