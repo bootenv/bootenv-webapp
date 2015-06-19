2cf59dfb56808c84c029ee9d0ea9753a20eeaf36
César Izurieta <cesar@caih.org>
2015-06-18 22:41:45 -0300

    Updated versions and minor changes

---
+++ tests/unit/helpers/hex2rgb-fg-test.js
@@ -0,0 +1,10 @@
+import { hex2rgbFg } from '../../../helpers/hex2rgb-fg';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | hex2rgb fg');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = hex2rgbFg(42);
+  assert.ok(result);
+});
